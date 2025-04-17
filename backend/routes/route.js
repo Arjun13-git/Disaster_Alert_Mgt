6a714633b75921ea499c46f10cecdddd7dc34c0c
samithspalan <samithpoojary12345@gmail.com>
2025-04-17 23:49:25 +0530

    added middlewares

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,6 +1,6 @@
 import express, { Router } from 'express';
 import { getUser, login, logout, singUp } from '../controller/auth.js';
-import { checkAuth } from '../middlewares/checkauth.js';
+import { checkAuth } from '../middlewares/checkAuth.js';
 const authRouter =  express.Router();
 authRouter.post('/signup',(singUp))
 authRouter.post('/login',(login))
