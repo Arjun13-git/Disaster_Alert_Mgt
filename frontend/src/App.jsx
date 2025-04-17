2ceb2dd11a5ed8bec0a79020d7c588d8e1f74ac5
samithspalan <samithpoojary12345@gmail.com>
2025-04-17 23:16:24 +0530

    removed navbar from login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
 import Home from './pages/Home';
 import Location from './pages/Location';
 import Profile from './pages/Profile';
 import SOS from './pages/SOS';
-import './App.css'
+import './App.css';
 import LoginPage from './pages/login';
 import SignupPage from './pages/signup';
+
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+  </>
+);
 
 function App() {
   return (
     <Router>
-      <Navbar/>
       <Routes>
-        <Route path="/" element={<LandingPage/>}/>
-        <Route path="/signup" element={<SignupPage/>}/>
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/location" element={<Location/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/sos" element={<SOS/>} />
-      </Routes>
-    </Router>  )
-}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
 
+
+        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/home" element={<Layout><Home /></Layout>} />
+        <Route path="/location" element={<Layout><Location /></Layout>} />
+        <Route path="/profile" element={<Layout><Profile /></Layout>} />
+        <Route path="/sos" element={<Layout><SOS /></Layout>} />
+      </Routes>
+    </Router>
+  );
+}
 export default App
